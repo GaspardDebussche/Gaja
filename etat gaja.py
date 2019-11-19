1393ccfe7ab6515e261eabca46529c8340d948f6
anaishoareau <49558242+anaishoareau@users.noreply.github.com>
2019-11-19 10:16:06 +0100

    Create etat gaja.py

---
+++ etat gaja.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 09:30:36 2019
+
+@author: 33766
+"""
+
+#Gaja's states
+#0,1,2,3,4,5,6
+#N,p,eb,er,sp,si,sn
+#Neurtral, give the paw, blue eyes, red eyes, positive sound, interrogative sound, negative sound
+
+#Labelisation of Gaja's states
+#None,-2,-1,0,1
+#Impossible,not interesting,negative,interrogative,positive
+
+# 
