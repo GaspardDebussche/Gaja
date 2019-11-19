81262c600fedef26685cbdbddff2f9d3a2d289ee
anaishoareau <49558242+anaishoareau@users.noreply.github.com>
2019-11-19 10:22:14 +0100

    Update etat gaja.py

--- etat gaja.py
+++ etat gaja.py
@@ -14,4 +14,8 @@ Created on Tue Nov 19 09:30:36 2019
 #None,-2,-1,0,1
 #Impossible,not interesting,negative,interrogative,positive
 
-# 
+import pandas as pd
+
+dataset = pd.read_csv("./data/labelized_gaja_states.csv")
+
+print(dataset.head())
