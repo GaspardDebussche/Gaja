76deb55530e3206a86816f398220cbad2cd9249b
anaishoareau <49558242+anaishoareau@users.noreply.github.com>
2019-11-27 14:51:59 +0100

    Update etat gaja.py

--- etat gaja.py
+++ etat gaja.py
@@ -10,12 +10,88 @@ Created on Tue Nov 19 09:30:36 2019
 #N,p,eb,er,sp,si,sn
 #Neurtral, give the paw, blue eyes, red eyes, positive sound, interrogative sound, negative sound
 
-#Labelisation of Gaja's states
+#Label of Gaja's states
 #None,-2,-1,0,1
 #Impossible,not interesting,negative,interrogative,positive
 
 import pandas as pd
+import numpy as np
 
 dataset = pd.read_csv("./data/labelized_gaja_states.csv")
 
-print(dataset.head())
+print(dataset)
+
+# transition matrix + reward
+
+# TRANSITION MATRIX RULES
+
+transition_matrix_positive = []
+
+transition_matrix_positive = np.zeros((len(dataset), len(dataset)))
+
+for i in range(len(dataset)):
+    for j in range(len(dataset)):
+        if dataset['label'][i] == 1 and dataset['label'][j] == 1:
+            transition_matrix_positive[i][j] = 1
+        elif dataset['label'][i] == -1 and dataset['label'][j] == 0:
+            transition_matrix_positive[i][j] = 1
+        elif dataset['label'][i] == 0 and dataset['label'][j] == 1:
+            transition_matrix_positive[i][j] = 1
+        else:
+            transition_matrix_positive[i][j] = 0
+            
+#print(transition_matrix_positive)
+
+transition_matrix_negative = []
+
+transition_matrix_negative = np.zeros((len(dataset), len(dataset)))
+
+for i in range(len(dataset)):
+    for j in range(len(dataset)):
+        if dataset['label'][i] == 1 and dataset['label'][j] == 0:
+            transition_matrix_negative[i][j] = 1
+        elif dataset['label'][i] == -1 and dataset['label'][j] == -1:
+            transition_matrix_negative[i][j] = 1
+        elif dataset['label'][i] == 0 and dataset['label'][j] == -1:
+            transition_matrix_negative[i][j] = 1
+        else:
+            transition_matrix_negative[i][j] = 0
+            
+#print(transition_matrix_negative)
+
+
+transition_matrix_neutral = []
+
+transition_matrix_neutral = np.zeros((len(dataset), len(dataset)))
+
+for i in range(len(dataset)):
+    for j in range(len(dataset)):
+        if dataset['label'][i] == 1 and dataset['label'][j] == 0:
+            transition_matrix_neutral[i][j] = 1
+        elif dataset['label'][i] == -1 and dataset['label'][j] == 0:
+            transition_matrix_neutral[i][j] = 1
+        elif dataset['label'][i] == 0 and dataset['label'][j] == 0:
+            transition_matrix_neutral[i][j] = 1
+        else:
+            transition_matrix_neutral[i][j] = 0
+            
+print(transition_matrix_neutral)
+
+reward_be_friendly = []
+
+reward_be_friendly = np.zeros((len(dataset), len(dataset)))
+
+for i in range(len(dataset)):
+    for j in range(len(dataset)):
+        if j == 18:
+            reward_be_friendly[i][j] = 10
+        elif dataset['label'][i] == 1 and dataset['label'][j] == 1:
+            reward_be_friendly[i][j] = -2
+        elif dataset['label'][i] == 0 and dataset['label'][j] == 1:
+            reward_be_friendly[i][j] = -2
+        elif dataset['label'][i] == -1 and dataset['label'][j] == 0:
+            reward_be_friendly[i][j] = -2
+        else:
+            reward_be_friendly[i][j] = -5
+            
+print(reward_be_friendly)
