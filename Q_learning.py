d50c1da3c00a48175338ededae7ada00c8291168
anaishoareau <49558242+anaishoareau@users.noreply.github.com>
2019-11-28 15:58:28 +0100

    Update Q_learning.py

--- Q_learning.py
+++ Q_learning.py
@@ -64,7 +64,7 @@ for j in range(len(dataset)*3):
             else:
                 transition_matrix[i][j] = 0
 
-# Stochastic
+# Stochastication
 for i in range(len(transition_matrix)):
     s=0
     for j in range(len(transition_matrix)):
@@ -73,26 +73,32 @@ for i in range(len(transition_matrix)):
     if s>0:
         for p in range(len(transition_matrix)):
             transition_matrix[i][p] = transition_matrix[i][p] / s
-        
-        
+    else:
+        transition_matrix[i][1] = 1
+for i in range(len(transition_matrix)):
+    print(sum(transition_matrix[i][:]))
+    
 # Reward : 
 # 10 : the most positive triplet -> [i,18]
 # -2 : -1 -> 0 ; 0 -> 1
 # -5 : 1 -> 0 ; -1 -> 0 
 
 reward_matrix = []
-reward_matrix = np.zeros((len(dataset), len(dataset)))
+reward_matrix = np.zeros((len(dataset)*3, len(dataset)*3))
 
-for i in range(len(dataset)):
-    for j in range(len(dataset)):
-        if j == 18:
-            reward_matrix[i][j] = 10
-        elif dataset['label'][i] == 1 and dataset['label'][j] == 1:
-            reward_matrix[i][j] = -2
-        elif dataset['label'][i] == 0 and dataset['label'][j] == 1:
-            reward_matrix[i][j] = -2
-        elif dataset['label'][i] == -1 and dataset['label'][j] == 0:
-            reward_matrix[i][j] = -2
+for i in range(len(dataset)*3):
+    for j in range(len(dataset)*3):
+        if (0 <= i <= 23 and 0 <= j <= 23) or (24 <= i <= 48 and 24 <= j <= 48) or (49 <= i <= 72 and 49 <= j <= 72):
+            if j%24 == 18:
+                reward_matrix[i][j] = 10
+            elif dataset['label'][i%24] == 1 and dataset['label'][j%24] == 1:
+                reward_matrix[i][j] = -2
+            elif dataset['label'][i%24] == 0 and dataset['label'][j%24] == 1:
+                reward_matrix[i][j] = -2
+            elif dataset['label'][i%24] == -1 and dataset['label'][j%24] == 0:
+                reward_matrix[i][j] = -2
+            else:
+                reward_matrix[i][j] = -5
         else:
             reward_matrix[i][j] = -5
 
@@ -103,7 +109,7 @@ print(reward_matrix)
 
 state_size, action_size = transition_matrix.shape
 
-total_episodes = 50000
+total_episodes = 1000  #50000
 total_test_episodes = 100
 max_steps = 99
 
@@ -123,17 +129,37 @@ qtable = np.zeros((state_size, action_size))
 start_training = time()
 count_1, count_2 = 0, 0
 for episode in tqdm(range(total_episodes)):
-    state = random.randint(0, state_size-1)
     
+    # polarity definition
+    p = 1
+    polarity = random.random()
+    if 0 <= polarity < 0.50:
+        p = 1
+    elif 0.5 <= polarity < 0.75:
+        p = 0
+    else:
+        p = -1
+    # gaja's state definition
+    gaja_state = random.randint(0, 23)
+    # global state defintion
+    if p == 1:
+        state = gaja_state + 49
+    elif p == 0:
+        state = gaja_state + 24
+    else:
+        state = gaja_state
+
     for step in range(max_steps):
 
         exp_exp_tradeoff = random.uniform(0,1)
 
         if exp_exp_tradeoff > epsilon:
+            #Prend la meilleur action en fonction de l'état décidé avant (global state definition)
             action = np.argmax(qtable[state,:])
             count_1 += 1
         
         else:
+            # Choisit une action aléatoire parmi les possibles
             action = np.random.choice([i for i in range(state_size)], p=transition_matrix[state,:])
             count_2 += 1
 
