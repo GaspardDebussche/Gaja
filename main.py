c620148752d1e3439b58d1dde4f50c01e0721ddb
Anais <anaishoareau@yahoo.fr>
2020-01-17 15:11:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from Reinforce import reinforcement_learning
 from speech import sentence_transcription
 from reward_matrix import dataset
+from time import time
 
 sentence = sentence_transcription()
+t=time()
 reinforce_variables = reinforcement_learning(sentence)
-
+print(time()-t)
 for i in reinforce_variables[0]:
     print(dataset.iloc[i])
