fe559f1eeb05da1a1a8f97826890973a916618d1
Anais <anaishoareau@yahoo.fr>
2020-01-17 15:11:41 +0100

    Update speech.py

--- speech.py
+++ speech.py
@@ -29,7 +29,6 @@ import speech_recognition as sr
 
 def recognize_speech_from_mic(recognizer, microphone):
     """Transcribe speech from recorded from `microphone`.
-
     Returns a dictionary with three keys:
     "success": a boolean indicating whether or not the API request was
                successful
@@ -49,10 +48,7 @@ def recognize_speech_from_mic(recognizer, microphone):
     # adjust the recognizer sensitivity to ambient noise and record audio
     # from the microphone
     with microphone as source:
-        #recognizer.adjust_for_ambient_noise(source) 
-        recognizer.pause_threshold=1
-        #recognizer.adjust_for_ambient_noise(source,duration=1)
-        recognizer.adjust_for_ambient_noise(source) 
+        recognizer.adjust_for_ambient_noise(source) # #  analyze the audio source for 1 second
         audio = recognizer.listen(source)
 
     # set up the response object
@@ -64,13 +60,13 @@ def recognize_speech_from_mic(recognizer, microphone):
 
     # try recognizing the speech in the recording
     # if a RequestError or UnknownValueError exception is caught,
-    #     update the response object accordingly
+    #   update the response object accordingly
     try:
         response["transcription"] = recognizer.recognize_google(audio)
     except sr.RequestError:
         # API was unreachable or unresponsive
         response["success"] = False
-        response["error"] = "API unavailable"
+        response["error"] = "API unavailable/unresponsive"
     except sr.UnknownValueError:
         # speech was unintelligible
         response["error"] = "Unable to recognize speech"
