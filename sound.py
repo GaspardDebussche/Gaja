131179db7abf1b8dbf85f35f7eedb8f201586036
Gaspard <gaspard.debussche@gmail.com>
2020-01-18 14:58:44 +0100

    minor changes

--- sound.py
+++ sound.py
@@ -3,8 +3,6 @@ import pygame
 pygame.mixer.init()
 
 
-#while pygame.mixer.music.get_busy() == True:
-#    pass
 
 
 def sound_management(state):
@@ -13,13 +11,20 @@ def sound_management(state):
         pygame.mixer.music.load("/home/pi/Documents/Gaja/sounds/bad.mp3")
         pygame.mixer.music.set_volume(1.0)
         pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            pass
 
     if state==1:
         pygame.mixer.music.load("/home/pi/Documents/Gaja/sounds/normal.mp3")
         pygame.mixer.music.set_volume(1.0)
         pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            pass
+        
 
     if state==2:
         pygame.mixer.music.load("/home/pi/Documents/Gaja/sounds/good.mp3")
         pygame.mixer.music.set_volume(1.0)
         pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            pass
