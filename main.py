4c4df35ba4d70a95707d74207a518f0cc01876ba
Anais <anaishoareau@yahoo.fr>
2020-01-20 11:41:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+""" IMPORTS """
+
 from Reinforce import reinforcement_learning
 from speech import sentence_transcription
 from reward_matrix import dataset
@@ -5,7 +9,11 @@ import time
 from LED import led_management
 from sound import sound_management
 
+
+""" RUNNING CODE """
+
 state = 0
+
 while True:
     sentence = sentence_transcription()
     t=time.time()
