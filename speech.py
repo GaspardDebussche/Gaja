98a3abe9c1d6d3560e622e6f6943d0f7a1d85cb2
Anais <anaishoareau@yahoo.fr>
2020-01-20 11:40:59 +0100

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,34 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 27 15:02:09 2019
 
-@author: 33766
-"""
-#
-#import speech_recognition as sr
-#
-#r = sr.Recognizer()
-#mic = sr.Microphone()
-
-#If your system has no default microphone (such as on a RaspberryPi), 
-#or you want to use a microphone other than the default, you will need 
-#to specify which one to use by supplying a device index. You can get 
-#a list of microphone names by calling the list_microphone_names()
-#static method of the Microphone class.
-
-
+""" IMPORTS """
 import speech_recognition as sr
 
 
-
-#print(sr.Microphone.list_microphone_names())
-#mic = sr.Microphone(device_index=3)
-
-#with mic as source:
-#    r.adjust_for_ambient_noise(source)
-#    audio = r.listen(source)
-#
-#r.recognize_google(audio)
+""" SPEECH RECOGNITION """
 
 def recognize_speech_from_mic(recognizer, microphone):
     """Transcribe speech from recorded from `microphone`.
